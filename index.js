ddd654e8c2c33172cc5be468d5050c22a3f3ed46
mitchell williams <mscottwilliams1994@gmail.com>
2019-05-15 20:34:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
-var uppercase = " HELLO "
-uppercase.toUpperCase() === uppercase
-var lowercase = 'hello'
-lowercase.toLowerCase() === lowercase
-var mixedcase = 'Hi there!'
-mixedcase.toUpperCase() === mixedcase
-mixedcase.toLowerCase() === mixedcase
 function shout(string) {
-  return string.toUpperCase()
+  return string.toUpperCase() 
 }
 function whisper(string) {
   return string.toLowerCase()
 }
+var uppercase = "HELLO"
+uppercase.toUpperCase() === uppercase 
+var lowercase = 'hello'
+lowercase.toLowerCase() === lowercase
+var mixedCase = 'Hi there!'
+mixedCase.toLowerCase() === mixedCase
+mixedCase.toUpperCase() === mixedCase
 function logShout(string) {
   console.log(uppercase)
 }
+function logWhisper(string) {
+  console.log(lowercase)
+}
+
+
+
+
+function sayHiToGrandma(string) {
+ 
+ if (string.toLowerCase() === string) {
+   return "I can't hear you!"
+ }  
+  if (string.toUpperCase()=== string) {
+    return "YES INDEED!"
+  }
+  if (string === "I love you, Grandma.") {
+    return "I love you, too."
+  }}
